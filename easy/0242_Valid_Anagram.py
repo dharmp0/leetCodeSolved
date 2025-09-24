aa1935e8bd3a6db5ba604dbeddb4d8bedc122965
Dharm P <dharmpat@outlook.com>
2025-09-24 03:40:41 -0400

    0242_Valid_Anagram

--- easy/0242_Valid_Anagram.py
+++ easy/0242_Valid_Anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        freq = {}
+
+        for i in s:
+            freq[i] = freq.get(i, 0) + 1
+        
+        for n in t:
+            if n not in freq:
+                return False
+            freq[n] -= 1
+            if freq[n] == 0:
+                del freq[n]
+        
+        return len(freq) == 0
